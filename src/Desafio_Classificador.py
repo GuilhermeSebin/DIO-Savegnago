054c07dd91086e5bc3528ae3f92aba731640b309
GuilhermeSebin <guilhermesebin@outlook.com>
2025-08-07 17:32:00 -0300

    Entrega de Desafio

---
+++ src/Desafio_Classificador.py
@@ -0,0 +1,24 @@
+#Desafio Classificador de Nível de Herói
+
+heroi = "Hiero"
+xp = int(input("Quanto xp tem nosso herói?: "))
+
+if xp <= 1000:
+    nivel = "Ferro"
+elif 1001 <= xp <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+print(f"O Herói de nome {heroi} está no nível {nivel}")
+
